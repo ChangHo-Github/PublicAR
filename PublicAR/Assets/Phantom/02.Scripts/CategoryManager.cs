8ab55922acf88dd9eedacfb485216acaa2f25213
LeeChangHo-James <chho1365@gmail.com>
2022-01-11 18:40:53 +0900

    2022-01-11

---
+++ PublicAR/Assets/Phantom/02.Scripts/CategoryManager.cs
@@ -0,0 +1,13 @@
+using Cysharp.Threading.Tasks;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CategoryManager : MonoBehaviour
+{
+    async UniTask Start()
+    {
+        ScrollRect scroll = GetComponent<ScrollRect>();
+    }
+}
